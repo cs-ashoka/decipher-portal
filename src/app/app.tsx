46a13c9a51497bd8db0c8e6b3a972f5c95c65b99
Rajat Varma <rajatvarma08@gmail.com>
2023-03-18 00:25:47 +0530

    things were setup

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import React from 'react';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="decipher-portal" />
-
-      <div />
-    </>
+    // authentication system
+    <div className='page'>
+        <h1>Login</h1>
+        <form>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" />
+            <br />
+            <label style={{marginLeft: 10}} htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+            <br />
+            <button type="submit">Login</button>
+        </form>
+    </div>
   );
 }
 
