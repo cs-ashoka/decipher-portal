e08d5a3b67058dca0f0be2a359e4f5e87ad8faf9
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-02-24 00:06:38 +0530

    safe open

--- src/app/rooms/Cryptography.tsx
+++ src/app/rooms/Cryptography.tsx
@@ -6,12 +6,6 @@ import { useNavigate } from 'react-router-dom';
 const BACKEND_URL = 'https://decipher-backend.onrender.com';
 
 export default function CryptographyRoom() {
-    const [progress, setProgress] = React.useState({
-        0: false,
-        1: false,
-        2: false,
-        3: false
-    });
 
     const [currentChallenge, setCurrentChallenge] = useState(1);
     const [modal, setModal] = React.useState(0);
@@ -19,6 +13,7 @@ export default function CryptographyRoom() {
     const navigate = useNavigate()
     const [error, setError] = useState(false);
     const [warning, setWarning] = useState(false);
+    const [solved, setSolved] = useState(false);
 
     const solve = (i:number, answer:string) => {
         console.log(i, answer)
@@ -31,7 +26,6 @@ export default function CryptographyRoom() {
             console.log(i)
             if (res.data)
              {
-            setProgress(progress => ({...progress, [i]: true}))
             setModal(0)
             // setCurrentChallenge(currentChallenge + 1);
             //             console.log(currentChallenge)
@@ -58,6 +52,7 @@ export default function CryptographyRoom() {
             try {
                 const response = await axios.post(`${BACKEND_URL}/play/2`, {}, {withCredentials: true});
                 setQuestion(response.data.question);
+                setError(false)
             } catch (error) {
                 setQuestion('Failed to fetch question');
             }
@@ -66,18 +61,19 @@ export default function CryptographyRoom() {
         fetchQuestion();
     }, []);
         return(
-            <div style={{
-                position: "absolute", zIndex: 100, width: '100vw', height: '100vh', display: "flex", backgroundColor: '#00000099', justifyContent: "center", alignItems: "center",
-                flexDirection: "column"
-            }}>
-                <div style={{textDecoration: 'underline', cursor: 'pointer', marginBottom: 30}} onClick={() => setSafeOpen(false)}>Close</div>
-                <div style={{backgroundColor: '#999', display: "flex", border: '#BBB 10px solid', width: '30vw', height: '50vh', justifyContent: "center", alignItems: "center" }}>
-                    <div style={{backgroundColor: "white", height: "30vh", width: "15vw", color: "black", padding: 10, textAlign: 'center'}}>
-                        <p style={{fontFamily: 'monospace'}}>{question}</p>
-                    </div>
+            <div
+                style={{
+                    position: "absolute", zIndex: 20, width: '100vw', height: '100vh', display: "flex", backgroundColor: '#00000099', justifyContent: "center", alignItems: "center",
+                    flexDirection: "column"
+                }}>
+                <div style={{ padding:"20px", width: 'fit-content', color: "black", height: 'fit-content', backgroundColor: "white", display: "flex", justifyContent: "space-evenly", alignItems: "center", flexDirection: "column" }}>
+                    <div onClick={() => setSafeOpen(false)} style={{ cursor: "pointer" }}>Close</div>
+                    <div style={{ display: error ? 'block' : 'none', margin: 5 }}>Incorrect answer</div>
+                    <h3 style={{ margin: 5 }}>{question}</h3>
+                    <input style={{ margin: 5 }} value={key} onChange={(e) => setKey(e.target.value)} type="text" />
+                    <button style={{ margin: 5 }} onClick={() => {
+                    solve(currentChallenge, key)}}>Submit</button>
                 </div>
-                <input style={{marginTop: 25}} type="text" name="" id="" value={key} onChange={(e) => setKey(e.target.value)} />
-                <button style={{margin : 10}} onClick={() => {solve(3, key)}}>Submit</button>
             </div>
         )
     }
@@ -86,9 +82,6 @@ export default function CryptographyRoom() {
         const [key, setKey] = useState('');
         const [question, setQuestion] = useState('');
         setError(false)
-        if (progress[0] && progress[1] && progress[2]) {
-            setWarning(true)
-        }
 
     useEffect(() => {
         const fetchQuestion = async () => {
@@ -97,8 +90,14 @@ export default function CryptographyRoom() {
                 console.log(response)
                 setQuestion(response.data.question);
                 setCurrentChallenge(response.data.challengeNumber);
+                if (currentChallenge == 3) {
+                    setWarning(true)
+                }
             } catch (error: any) {
-                if (error.code === "ERR_BAD_REQUEST") setQuestion('You have already solved this challenge')
+                if (error.code === "ERR_BAD_REQUEST") {
+                    setQuestion('You have already solved this challenge')
+                    setSolved(true);
+                }
                 else setQuestion('Failed to fetch question');
             }
         };
@@ -116,9 +115,9 @@ export default function CryptographyRoom() {
                     <div onClick={() => setModal(0)} style={{ cursor: "pointer" }}>Close</div>
                     <div style={{ color: "red", display: error ? 'block' : 'none', margin: 0 }}>Incorrect answer</div>
                     <div style={{ color: "red", display: warning ? 'block' : 'none', margin: 0 }}>Open the safe</div>
-                    <h3 style={{ margin: 0 }}>{question}</h3>
-                    <input value={key} onChange={(e) => setKey(e.target.value)} type="text" />
-                    <button onClick={() => {
+                    <h3 style={{ display: warning ? 'none' : 'block', margin: 5 }}>{question}</h3>
+                    <input style={{ display: warning || solved ? 'none' : 'block', margin: 5 }} value={key} onChange={(e) => setKey(e.target.value)} type="text" />
+                    <button style={{ display: warning || solved ? 'none' : 'block', margin: 5 }} onClick={() => {
                     solve(currentChallenge, key)}}>Submit</button>
                 </div>
             </div>
@@ -131,7 +130,7 @@ export default function CryptographyRoom() {
             {modal !== 0  && <SafeModal a={modal} />}
             <a href="/home" style={{color: "white", alignSelf: 'baseline', marginLeft: 25}}>← Back</a>
             <div style={{display: "flex"}}>
-                <p onClick={() => {if(progress[0] && progress[1] && progress[2]) {setSafeOpen(true)}}} style={{height: 10, fontWeight: 700, transform: 'rotate(-30deg)', position: "relative", top: 250, left: 240, cursor: 'pointer', fontSize: '0.5rem', zIndex: 10}}>Open the safe</p>
+                <p onClick={() => {if(currentChallenge == 3) {setSafeOpen(true)}}} style={{height: 10, fontWeight: 700, transform: 'rotate(-30deg)', position: "relative", top: 250, left: 240, cursor: 'pointer', fontSize: '0.5rem', zIndex: 10}}>Open the safe</p>
                 <img src={'../../assets/images/cryptographyroom.jpg'} height={600}>
                 {/* <object data={roomImage} type="image/svg+xml" height={600}> */}
                 </img>
